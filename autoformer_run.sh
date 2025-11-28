d38f5a90ddd04c48712442d03b2a4fc12ae62cc5
WearTheClo <67886325+WearTheClo@users.noreply.github.com>
2025-11-28 13:44:16 +0800

    Add files via upload

---
+++ autoformer_run.sh
@@ -0,0 +1,39 @@
+python run.py --model Autoformer --data ETTh1 --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTh1 --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTh1 --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTh1 --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data ETTh2 --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTh2 --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTh2 --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTh2 --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data ETTm1 --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTm1 --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTm1 --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTm1 --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data ETTm2 --features M --target OT --seq_len 96 --pred_len 96 --factor 1 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTm2 --features M --target OT --seq_len 96 --pred_len 192 --factor 1 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTm2 --features M --target OT --seq_len 96 --pred_len 336 --factor 1 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data ETTm2 --features M --target OT --seq_len 96 --pred_len 720 --factor 1 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data electricity --root_path ./dataset/electricity/ --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1 
+python run.py --model Autoformer --data electricity --root_path ./dataset/electricity/ --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data electricity --root_path ./dataset/electricity/ --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data electricity --root_path ./dataset/electricity/ --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data exchange_rate --root_path ./dataset/exchange_rate/ --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data exchange_rate --root_path ./dataset/exchange_rate/ --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data exchange_rate --root_path ./dataset/exchange_rate/ --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data exchange_rate --root_path ./dataset/exchange_rate/ --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data traffic --root_path ./dataset/traffic/ --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data traffic --root_path ./dataset/traffic/ --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data traffic --root_path ./dataset/traffic/ --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data traffic --root_path ./dataset/traffic/ --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
+
+python run.py --model Autoformer --data weather --root_path ./dataset/weather/ --features M --target OT --seq_len 96 --pred_len 96 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data weather --root_path ./dataset/weather/ --features M --target OT --seq_len 96 --pred_len 192 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data weather --root_path ./dataset/weather/ --features M --target OT --seq_len 96 --pred_len 336 --factor 3 --en_layers 2 --de_layers 1
+python run.py --model Autoformer --data weather --root_path ./dataset/weather/ --features M --target OT --seq_len 96 --pred_len 720 --factor 3 --en_layers 2 --de_layers 1
